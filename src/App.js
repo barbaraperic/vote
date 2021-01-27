498e3bb83331fbec8546dd95e2e9cb90f9e54e25
Barbara Peric <b.peric10@gmail.com>
2021-01-27 16:12:04 +0100

    WIP: Redux state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useSelector } from 'react-redux'
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const state = useSelector((state) => state)
+
+  console.log('state', state)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Vote</p>
     </div>
   );
 }
